d18479822a0822f497a14d4cc0d71108a29c4cb5
OlesyaNay <110473421+OlesyaNay@users.noreply.github.com>
2024-10-27 14:44:26 -0300

    Refactoring testRatatypePrintSpeed test to POM

--- src/test/java/TypingTutorTest.java
+++ src/test/java/TypingTutorTest.java
@@ -3,6 +3,7 @@ import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
+import runner.TestUtils;
 
 import java.awt.*;
 
@@ -14,28 +15,13 @@ public class TypingTutorTest extends BaseTest {
     // в ранее открытом окне (в IDE) пока не закончится текст или пока не будет остановлен тест.
     // !!! !!! !!!
     @Test
-    public void testTextTypingWithRobot() throws AWTException {
+    public void testRatatypePrintSpeed() throws AWTException {
         getDriver().get("https://www.ratatype.com/typing-tutor/");
 
-        Robot robot = new Robot();
-        robot.delay(3000);      // Задержка в 3 секунды перед началом работы робота.
-
-        // Фраза "type me to find out how many words per minute you can type", которую нужно напечатать,
-        // закодирована числовыми кодами клавиш для раскладки клавиатуры QWERTY
-        // ('t'=84; 'y'=89; ' '=32 и т.д.)
-        Integer[] intArray = {
-                84,89,80,69,32, 77,69,32, 84,79,32, 70,73,78,68,32, 79,85,84,32, 72,79,87,32, 77,65,78,89,32,
-                87,79,82,68,83,32, 80,69,82,32, 77,73,78,85,84,69,32, 89,79,85,32, 67,65,78,32, 84,89,80,69
-        };
-
-        // Генерация события нажатия клавиши для написания букв в раскладке QWERTY
-        for(int i = 0; i < intArray.length; i++) {
-            robot.delay(120);
-            robot.keyPress(intArray[i]);
-        }
+        String textForTyping = "type me to find out how many words per minute you can type";
+        TestUtils.startTypingWithRobot(textForTyping);
 
         WebElement titleH2 = getDriver().findElement(By.xpath("//h2"));
-
         Assert.assertTrue(titleH2.getText().contains("words per minute"));
     }
 
